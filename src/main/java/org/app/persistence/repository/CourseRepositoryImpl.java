a73c04a1be5ea83ac72ffcacda757733afb98055
= <=>
2025-01-18 13:46:09 +0100

    Die Entität wurde in den Kontext eingefügt.

--- src/main/java/org/app/persistence/repository/CourseRepositoryImpl.java
+++ src/main/java/org/app/persistence/repository/CourseRepositoryImpl.java
@@ -13,17 +13,17 @@ public class CourseRepositoryImpl implements ICourseRepository{
 
     @Inject
     private EntityManager entityManager;
-
+    @Transactional
     @Override
     public List<Course> findAll() {
         return entityManager.createQuery("SELECT c FROM Course c").getResultList();
     }
-
+    @Transactional
     @Override
     public Course findById(Long id) {
         return entityManager.find(Course.class, id);
     }
-
+    @Transactional
     @Override
     public void save(Course course) {
         if(course.getId() != null && course.getId() > 0){
@@ -32,9 +32,16 @@ public class CourseRepositoryImpl implements ICourseRepository{
             this.entityManager.persist(course);
         }
     }
-
+    /*@Transactional
     @Override
     public void deleteById(Course course) {
         this.entityManager.remove(course);
+    }*/
+    @Transactional
+    @Override
+    public void deleteById(Course course) {
+        // Adjuntar la entidad al contexto si no está gestionada
+        Course managedCourse = this.entityManager.merge(course);
+        this.entityManager.remove(managedCourse);
     }
 }
